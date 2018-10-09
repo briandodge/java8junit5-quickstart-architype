4f11d1405646bc49a7be85bf710cc1f6343a582c
Brian <briandodge85@gmail.com>
2018-10-09 07:09:50 +0100

    Updated tests

--- src/main/resources/archetype-resources/src/test/java/__project-name__Test.java
+++ src/main/resources/archetype-resources/src/test/java/__project-name__Test.java
@@ -1,17 +1,33 @@
 package ${package};
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-class ${project-name}Test
-{
-    @Test
-    void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+@DisplayName("Given my application is running")
+@ExtendWith({/*Extend me*/})
+class ${project-name}Test {
+
+    @Nested
+    @DisplayName("When I want demo tests")
+    class DemoTests {
+
+        @Test
+        @DisplayName("Then assertTrue should return true")
+        void shouldAnswerWithTrue() {
+
+            assertAll(
+                    () -> assertTrue(true)
+            );
+
+        }
+
     }
 }
